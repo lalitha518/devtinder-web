30f6417ad6e71ee46bbbcf2160ba52c56d946461
Lalitha Bai <banavath.lalitha-bai@capgemini.com>
2025-04-11 11:11:42 +0530

    my first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React + Vite
+
+# DevTinder Web
+- created a Vite+React Project
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello React+Vite</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
